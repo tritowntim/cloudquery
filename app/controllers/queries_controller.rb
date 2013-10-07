8da8192184abf3b34d451e008f7b4a569f072588
Tim Dussinger <tritowntim@gmail.com>
2013-10-06 20:23:46 -0400

    Remove unused code; move helper to partial view

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -13,7 +13,6 @@ class QueriesController < ApplicationController
 
 	def recent
 		@queries = Query.order('created_at DESC').limit(50)
-		load_tables
 	end
 
 	def all
@@ -25,14 +24,9 @@ class QueriesController < ApplicationController
 		@query = Query.new
 		@resultset = nil
 		@results_count = 0
-		@table_list = db_tables.html_safe
+		@table_list = Metadata.list_tables(params['db_name'])
 
- 		# refresh = params[:force] == 'true'
- 		# puts refresh
- 		# @table_row_count = count_table_row(refresh)
-		# @table_row_counted_at = table_row_count_time
 		@table_row_counted_at = nil
-		# load_tables
 	end
 
 	def create
@@ -91,6 +85,7 @@ class QueriesController < ApplicationController
 		@results_count = results.count
 
 		@table_list = db_tables.html_safe
+		@table_list =
 
 		# TODO: support natively
 		# render :json => @results
@@ -110,24 +105,6 @@ class QueriesController < ApplicationController
 			"SELECT * FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE' ORDER BY table_name"
 		end
 
-		def db_tables
-			# tables = QueryDb.get_connection(params['db_name']).execute(sql_all_tables)
-			# table_row_counts = count_table_row(false)
-			table_list = "<p>Tables</p><ul>"
-      tables = Metadata.list_tables(params['db_name'])
-			tables.each do |table|
-				table_list += "<li>#{table.name}"
-				# if Metadata.exists?(name: table.name)
-        if table.record_count
-					table_list += " (#{ActionController::Base.helpers.number_with_delimiter(table.record_count)})"
-				else
-					table_list += " <em>N/A</em>"
-				end
-				table_list += "</li>"
-			end
-			table_list += "</ul>"
-		end
-
 		def oid_table_name
 			lookup = {}
 			pg_class = QueryDb.get_connection(params['db_name']).execute("SELECT relname, oid FROM pg_class")
@@ -135,45 +112,6 @@ class QueriesController < ApplicationController
 				lookup[c['oid'].to_i] = c['relname']
 			end
 			lookup
-		end
-
-		def count_table_row(force_refresh)
-			Rails.cache.fetch("table_row_count", :force => force_refresh) do
-				tables = QueryDb.get_connection(params['db_name']).execute("SELECT * FROM information_schema.tables WHERE table_schema = 'public' AND table_name LIKE '%pardot%' ORDER BY table_name")
-				table_row_counts = {}
-				tables.each do |table|
-					puts "counting #{table['table_name']} ..."
-					table_row_counts[table['table_name']] = QueryDb.get_connection(params['db_name']).execute("SELECT COUNT(1) FROM #{table['table_name']}")[0]['count']
-				end
-				Rails.cache.write("table_row_counted_at", DateTime.now)
-				table_row_counts
-			end
-		end
-
-		def table_row_count_time
-			Rails.cache.fetch("table_row_counted_at") do
-				count_table_row(false)
-				DateTime.now
-			end
-		end
-
-		def load_tables
-			tables = QueryDb.get_connection(params['db_name']).execute(sql_all_tables)
-			# Tables = {}
-			tables.each do |table|
-				puts "counting #{table['table_name']} ..."
-				row_count = QueryDb.get_connection(params['db_name']).execute("SELECT COUNT(1) FROM #{table['table_name']}")[0]['count']
-				size_bytes = QueryDb.get_connection(params['db_name']).execute("SELECT PG_TOTAL_RELATION_SIZE('#{table['table_name']}')")[0]['pg_total_relation_size']
-				if Metadata.exists?(name: table['table_name'])
-					m = Metadata.where(name: table['table_name']).first
-					m.record_count = row_count
-					m.size_bytes = size_bytes
-					m.save
-					m.touch
-				else
-					Metadata.create(object_type: 'table', schema: 'public', name: table['table_name'], record_count: row_count, size_bytes: size_bytes)
-				end
-			end
 		end
 
 		def query_params
