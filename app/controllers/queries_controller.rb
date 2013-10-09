4025dcf4c5d0240431764e6ba2f12a3ba16e5dbd
Tim Dussinger <tritowntim@gmail.com>
2013-10-09 07:42:19 -0400

    Purge remaining tabs

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -10,9 +10,9 @@ class QueriesController < ApplicationController
     manage_query
   end
 
-	def all
-		@queries = Query.order('created_at DESC')
-	end
+  def all
+    @queries = Query.order('created_at DESC')
+  end
 
   # TODO: load last query into text field
   def new
@@ -37,73 +37,73 @@ class QueriesController < ApplicationController
     end
   end
 
-	private
+  private
 
-	def manage_query
-		sql = @query.sql_text
+  def manage_query
+    sql = @query.sql_text
 
-		@results = execute_query(sql)
+    @results = execute_query(sql)
 
-		@query.record_count = @results['detail'].count
-		@query.db = @db_name
-		@query.save
+    @query.record_count = @results['detail'].count
+    @query.db = @db_name
+    @query.save
 
-		@new_query = Query.new
-		@new_query.sql_text = sql
-	end
+    @new_query = Query.new
+    @new_query.sql_text = sql
+  end
 
-	def execute_query(sql)
-		query_begin = Time.now.strftime('%s%3N').to_i
-		puts "DB QUERY BEGIN  #{Time.now}  #{}"
-		results = QueryDb.get_connection(@database.id).execute(sql)
-		puts "DB QUERY END  #{Time.now}  #{Time.now.strftime('%s%3N')}"
-		query_end = Time.now.strftime('%s%3N').to_i
-		@query.duration_ms = query_end - query_begin
+  def execute_query(sql)
+    query_begin = Time.now.strftime('%s%3N').to_i
+    puts "DB QUERY BEGIN  #{Time.now}  #{}"
+    results = QueryDb.get_connection(@database.id).execute(sql)
+    puts "DB QUERY END  #{Time.now}  #{Time.now.strftime('%s%3N')}"
+    query_end = Time.now.strftime('%s%3N').to_i
+    @query.duration_ms = query_end - query_begin
 
-		table_oid = oid_table_name
+    table_oid = oid_table_name
 
-		resultset = {}
-		resultset['header'] = []
-		header = resultset['header']
+    resultset = {}
+    resultset['header'] = []
+    header = resultset['header']
 
-		results.fields().each_index do |i|
-			head = {}
-			head['column_name'] = results.fname(i)
-			head['table_name'] = table_oid[results.ftable(i)]
-			head['data_type'] = QueryDb.get_connection(@database.id).execute("SELECT format_type(#{results.ftype(i)}, #{results.fmod(i)})").getvalue(0,0)
-			header << head
-		end
+    results.fields().each_index do |i|
+      head = {}
+      head['column_name'] = results.fname(i)
+      head['table_name'] = table_oid[results.ftable(i)]
+      head['data_type'] = QueryDb.get_connection(@database.id).execute("SELECT format_type(#{results.ftype(i)}, #{results.fmod(i)})").getvalue(0,0)
+      header << head
+    end
 
-		resultset['detail'] = []
-		detail = resultset['detail']
+    resultset['detail'] = []
+    detail = resultset['detail']
 
-  	results.each_row do |row|
- 			det = {}
- 			i = 0
-	    row.each do |v|
-	    	det["#{i}"] = v.to_s
-	    	i = i + 1
-	    end
-			detail << det
-		end
+    results.each_row do |row|
+      det = {}
+      i = 0
+      row.each do |v|
+        det["#{i}"] = v.to_s
+        i = i + 1
+      end
+      detail << det
+    end
 
-		resultset
-	end
+    resultset
+  end
 
-		def oid_table_name
-			lookup = {}
-			pg_class = QueryDb.get_connection(@database.id).execute("SELECT relname, oid FROM pg_class")
-			pg_class.each do |c|
-				lookup[c['oid'].to_i] = c['relname']
-			end
-			lookup
-		end
+  def oid_table_name
+    lookup = {}
+    pg_class = QueryDb.get_connection(@database.id).execute("SELECT relname, oid FROM pg_class")
+    pg_class.each do |c|
+      lookup[c['oid'].to_i] = c['relname']
+    end
+    lookup
+  end
 
-		def query_params
-	    params.require(:query).permit(:sql_text)
-		end
+  def query_params
+    params.require(:query).permit(:sql_text)
+  end
 
-		def table_list
-    	@table_list = Metadata.list_tables(params[:database_id])
-  	end
+  def table_list
+    @table_list = Metadata.list_tables(params[:database_id])
+  end
 end
