2e30a09c2b558e81b76398fe57867916efcf0538
Tim Dussinger <tritowntim@gmail.com>
2013-10-09 07:48:12 -0400

    Purge remaining tabs

--- app/controllers/metadatas_controller.rb
+++ app/controllers/metadatas_controller.rb
@@ -1,24 +1,24 @@
 class MetadatasController < ApplicationController
 
   # TODO: support all metadatas, perhaps with route not nested within database (?)
-	def index
+  def index
     @database = Database.find(params['database_id'])
-		@metadatas = @database.metadatas.order('size_bytes DESC')
-	end
+    @metadatas = @database.metadatas.order('size_bytes DESC')
+  end
 
   # TODO: restore columns support
-	def columns
-		sql = <<-SQL
-			    SELECT c.table_name, c.column_name, c.ordinal_position,
-			    			 c.is_nullable, c.data_type, c.character_maximum_length
-			      FROM information_schema.tables t
-			INNER JOIN information_schema.columns c ON t.table_name = c.table_name
-			     WHERE t.table_schema = 'public' AND table_type = 'BASE TABLE'
-			  ORDER BY c.table_name, c.ordinal_position
-		SQL
+  def columns
+    sql = <<-SQL
+          SELECT c.table_name, c.column_name, c.ordinal_position,
+                 c.is_nullable, c.data_type, c.character_maximum_length
+            FROM information_schema.tables t
+      INNER JOIN information_schema.columns c ON t.table_name = c.table_name
+           WHERE t.table_schema = 'public' AND table_type = 'BASE TABLE'
+        ORDER BY c.table_name, c.ordinal_position
+    SQL
 
-		@cols = QueryDb.connection.execute(sql)
-	end
+    @cols = QueryDb.connection.execute(sql)
+  end
 
   # TODO: accessible via rake task for scheduling some day
   def refresh
