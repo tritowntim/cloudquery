895710634b476b757de0f8d5490510c6fe67235d
Tim Dussinger <tritowntim@gmail.com>
2013-10-22 23:33:51 -0400

    Display TRUE or FALSE in small caps instead of 't' or 'f'; improve some view variable names

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+  def render_value(value, data_type=nil)
+    if data_type && data_type.downcase == 'boolean'
+      "<span class=\"column-boolean\">#{format_boolean(value)}</span>".html_safe
+    else
+      value
+    end
+  end
+
+  def format_boolean(value)
+    if value == nil
+      nil
+    elsif value == 't'
+      "true"
+    else
+      "false"
+    end
+  end
+
 end
