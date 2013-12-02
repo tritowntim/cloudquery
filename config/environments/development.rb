ef7285caa05430d2ac31d368234de46d0809eb9e
Tim Dussinger <tritowntim@gmail.com>
2013-12-01 16:00:26 -0800

    Remove Rails 4 depreciation and upgrade warnings during startup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,13 +1,13 @@
 Cloudquery::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  # Per warning after upgrade to Rails 4
+  config.eager_load = false
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
@@ -22,13 +22,6 @@ Cloudquery::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  # Raise exception on mass assignment protection for Active Record models
-  config.active_record.mass_assignment_sanitizer = :strict
-
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  config.active_record.auto_explain_threshold_in_seconds = 0.5
-
   # Do not compress assets
   config.assets.compress = false
 
@@ -37,6 +30,6 @@ Cloudquery::Application.configure do
 
   config.logger = Logger.new(STDOUT)
   config.logger.level = Logger.const_get(
-    ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'DEBUG' 
+    ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'DEBUG'
   )
 end
